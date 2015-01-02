0817db278471027a523e00b48312ae954a8a189a
Roland <roland.schmitt@web.de>
2015-01-02 16:49:48 +0100

    rails 4.2.0

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141116212741) do
 
-  create_table "contacts", force: true do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "message"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20141116212741) do
     t.datetime "updated_at"
   end
 
-  create_table "registrations", force: true do |t|
+  create_table "registrations", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.string   "street"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20141116212741) do
     t.boolean  "paid",       default: false
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
